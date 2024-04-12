f2a0818fb824c465fde49c8c19397bfd217d1b83
mainnet-pat <mainnet_pat@protonmail.com>
2024-04-12 12:51:43 +0000

    Update package.json, bump version

--- build.sh
+++ build.sh
@@ -24,12 +24,22 @@ PATH=/opt/homebrew/opt/llvm/bin:$PATH wasm-pack build --out-dir pkg-node --targe
 # Get the package name
 PKG_NAME=$(jq -r .name pkg/package.json | sed 's/\-/_/g')
 
+tee pkg/index.js <<EOF
+if (typeof globalThis.window !== "undefined") {
+  module.exports = require('./dleq_tools.js');
+} else {
+  module.exports = require('./dleq_tools_node.js');
+}
+EOF
+
 # Merge nodejs & browser packages
 cp "pkg-node/${PKG_NAME}.js" "pkg/${PKG_NAME}_node.js"
 cp "pkg-node/${PKG_NAME}_bg.wasm" "pkg/${PKG_NAME}_bg_node.wasm"
 sed -i '' "s/${PKG_NAME}_bg.wasm/${PKG_NAME}_bg_node.wasm/g" "pkg/${PKG_NAME}_node.js"
-jq ".files += [\"${PKG_NAME}_node.js\", \"${PKG_NAME}_bg_node.wasm\", \"README.md\"]" pkg/package.json \
-    | jq ".main = \"${PKG_NAME}_node.js\"" \
+jq ".files += [\"${PKG_NAME}_node.js\", \"${PKG_NAME}_bg_node.wasm\", \"index.js\", \"README.md\"]" pkg/package.json \
+    | jq ".main = \"index.js\"" \
+    | jq ".module = \"dleq_tools_node.js\"" \
+    | jq ".browser = \"dleq_tools.js\"" \
     | jq ".repository.url = \"https://github.com/mainnet-pat/dleq-tools\"" \
     | jq ".license = \"MIT\"" > pkg/temp.json
 mv pkg/temp.json pkg/package.json
